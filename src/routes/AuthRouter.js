6e296586c1f13bc68f6b37ff7b8e703494c61d6d
Rolando HR <rolando167@hotmail.com>
2024-05-09 10:16:23 +0200

    RH - Mod

--- src/routes/AuthRouter.js
+++ src/routes/AuthRouter.js
@@ -1,10 +1,25 @@
 import express from "express";
 const routes = express.Router();
 
+/*=============================================================================
+|   Modulo:  Auth
+|   Fecha: 09-05-2024
+|   Ruta Test:  http://localhost:4000/api/v1/auth/
+ *===========================================================================*/
+
 routes.get('/', function(req, res, next) {
     res.status(200).json({
         state: true,
         message: '✔️ Modulo Auth!!'
+    })
+});
+
+routes.post('/login' , function(req, res){
+    console.log(req.body);
+	console.log(req.params);
+    res.status(200).json({
+        state: true,
+        message: '✔️ Usuario Logeado'
     })
 });
 
