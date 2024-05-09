7d8a34d922dc9c505f0610c17f0eaf4d38e39017
Rolando HR <rolando167@hotmail.com>
2024-05-09 15:25:13 +0200

    RH - mod service

--- src/app/Services/AuthService.js
+++ src/app/Services/AuthService.js
@@ -8,12 +8,16 @@ const auth = () => {
      //Al existir usuario en Base de datos ...
      const { id : sub, name } = { id : "12340", name :"Luccario"}
 
-     const token = jwt.sign({
-         sub,
-         name,
-         exp: Date.now() + (2 * 60) * 1000
-     }, TOKEN_SECRET);
+     const token = crearFirma(sub, name);
     return {token};
+}
+
+const crearFirma = (sub, name) =>{
+    return jwt.sign({
+        sub,
+        name,
+        exp: Date.now() + (2 * 60) * 1000
+    }, TOKEN_SECRET);
 }
 
 const AuthService = {
