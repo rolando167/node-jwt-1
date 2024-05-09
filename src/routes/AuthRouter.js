cbfa822c13f80dad85bb79fa1649dce8505c3240
Rolando HR <rolando167@hotmail.com>
2024-05-09 10:03:50 +0200

    RH - MOD

--- src/routes/AuthRouter.js
+++ src/routes/AuthRouter.js
@@ -2,15 +2,10 @@ import express from "express";
 const routes = express.Router();
 
 routes.get('/', function(req, res, next) {
-    try {
-        res.status(200).json({
-            state: true,
-            message: 'funciona!'
-        })
-
-    } catch (error) {
-        res.status(400).json({message:"error"})
-    }
+    res.status(200).json({
+        state: true,
+        message: '✔️ Modulo Auth!!'
+    })
 });
 
 export default routes;
